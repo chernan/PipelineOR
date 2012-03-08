982d1024494f4e248b42e6f7f33379cf09ae3d0b
Celine Hernandez <celine.hernandez@isb-sib.ch>
2012-03-08 18:10:07 +0100

    Add relationships for pipeline, pipeline_item and status. Improved display of their lists.

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,5 +1,6 @@
 class Script < ActiveRecord::Base
 
 has_many :input_outputs
+has_many :pipeline_items
 
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,6 @@
 class Status < ActiveRecord::Base
+
+has_many :pipeline
+has_many :pipeline_item
+
 end
